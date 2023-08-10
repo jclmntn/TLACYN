d354da1a36fb7b6e63c539c894587a5c4c8f26a2
jclmntn <jclmntn@gmail.com>
2023-08-10 08:48:44 -0300

    Adicionando a solução de TwoCrystallBalls

--- algorithms/TwoCrystalBalls.py
+++ algorithms/TwoCrystalBalls.py
@@ -1,2 +1,14 @@
-def two_crystal_balls():
-    pass
+import math
+def two_crystal_balls(breaks: list[bool]) -> int:
+    jump_amount = math.floor(math.sqrt(len(breaks)))
+    
+    for i in range(jump_amount, len(breaks), jump_amount):
+        if breaks[i]:
+            break
+    
+    i -= jump_amount
+    for j in range(i, i + jump_amount):
+        if breaks[j]:
+            return j
+        
+    return -1
